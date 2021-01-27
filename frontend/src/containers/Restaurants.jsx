712fd8e887b72d9966574ac3a0e3520d028a83e6
vlsk3104 <vlsk3104@gmail.com>
2021-01-27 12:31:05 +0900

    APIのデータを画面に表示

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,7 +1,13 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
 import styled from 'styled-components';
 
 import { fetchRestaurants } from '../apis/restaurants';
+
+import {
+  initialState,
+  restaurantsActionTyps,
+  restaurantsReducer,
+} from '../reducers/restaurants';
 
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
@@ -25,10 +31,18 @@ const MainCover = styled.img`
 `;
 
 export const Restaurants = () => {
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
+
   useEffect(() => {
+    dispatch({ type: restaurantsActionTyps.FETCHING });
     fetchRestaurants()
     .then((data) =>
-      console.log(data)
+      dispatch({
+        type: restaurantsActionTyps.FETCH_SUCCESS,
+        payload: {
+          restaurants: data.restaurants
+        }
+      })
     )
   }, [])
 
@@ -40,6 +54,13 @@ export const Restaurants = () => {
       <MainCoverImageWrapper>
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
+      {
+        state.restaurantsList.map(restaurant =>
+          <div>
+            {restaurant.name}
+          </div>
+        )
+      }
     </Fragment>
   )
 }
